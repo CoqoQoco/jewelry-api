1200d63e98a1a5c54dd15a06279e92e7c3a05ff1
CoqoQoco <chaisuriya_geo@hotmail.com>
2024-11-17 22:29:33 +0700

    [upd]

--- Jewelry.Api/Jewelry.Api/Program.cs
+++ Jewelry.Api/Jewelry.Api/Program.cs
@@ -1,75 +1,109 @@
 using Jewelry.Api.Controllers;
 using Jewelry.Api.Extension;
-using Jewelry.Data.Context;
 using Jewelry.Service.Helper;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
-using NPOI.SS.Formula.Functions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers()
-    .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+    .AddNewtonsoftJson(options =>
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+    );
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// JWT Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!)
+            )
+        };
+    });
+
+// CORS
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowAnyOrigin", options =>
+        options.WithOrigins(
+            "http://localhost:7000",
+            "https://localhost:7001",
+            "http://localhost:5173"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+    );
+});
+
+// Learn more about configuring Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "jewelry.api", Version = "v1" });
     options.CustomSchemaIds(type => SwashbuckleSchemaHelper.GetSchemaId(type));
-});
 
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 //Register DB, Service 
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-//allow cors origin
-builder.Services.AddCors(c =>
-{
-    c.AddPolicy("AllowAnyOrigin", options => options.WithOrigins(
-                                        "http://localhost:7000",
-                                        "https://localhost:7001",
-                                        "http://localhost:5173"  // Frontend port ถ้ามี
-                                         )
-                          .AllowAnyOrigin()
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
-});
-
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 app.UseExceptionMiddleware();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseHttpsRedirection();
-
     app.UseSwagger();
     app.UseSwaggerUI();
-
-    //TODO: Install swagger ui to docker container
-    //app.UseSwaggerUI(c =>
-    //{
-    //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jewelry V1");
-    //});
 }
 
-app.UseCors("AllowAnyOrigin");
-
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAnyOrigin");
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.MapGet("/", async context =>
-{
-    await context.Response.WriteAsync("Welcome to Jewelry API!");
-});
+// Health check endpoint
+app.MapGet("/", () => "Welcome to Jewelry API!")
+   .WithName("Home")
+   .WithOpenApi();
 
 //Check service/db connection
 app.MapGet("/network-status", async context =>
